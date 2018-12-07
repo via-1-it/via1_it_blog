ff715dafdc0feabe35b11c693565139f1aa08d6a
omgitzjene <Nguy2534@umn.edu>
2018-12-06 23:03:50 -0600

    Was able to run once first cloned, but after following the video and adding urls and etc, it wouldn't run anymore

--- via1_it_blog/settings.py
+++ via1_it_blog/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'it_blog',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -55,8 +56,8 @@ ROOT_URLCONF = 'via1_it_blog.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')]
-        ,
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
